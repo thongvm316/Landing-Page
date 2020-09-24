895d0319a946401f59b18de1d33798cd587bbcae
thongvm316 <vominhthong1603@gmail.com>
2020-09-24 16:31:30 +0700

    20200924

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 let bar = document.querySelector('.bar');
-let ul = document.querySelector('#header .container .nav-bar .nav-list ul')
+let ul = document.querySelector('#header .container .nav-bar .nav-list ul');
+let header = document.querySelector('section#header');
+
 bar.addEventListener('click', () => {
-    ul.classList.toggle('active')
+  ul.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 250) {
+        header.style.backgroundColor = '#29323c'
+    } else {
+        header.style.backgroundColor = 'transparent'
+    }
 });
